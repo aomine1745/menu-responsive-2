1fed5009a26baa37e337343c16ab9bb57c095fb9
aomine <xomarxpl07@outlook.es>
2019-03-07 20:08:43 -0500

    v 1.2

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,25 @@
 var btn = document.getElementById('menu'),
 links = document.getElementById('links'),
-body = document.querySelector('body'),
+wall = document.querySelector('.wall'),
 submenu = document.getElementsByClassName('subMenu');
 btn.addEventListener("click", ()=>{
 	if(links.classList.contains('active')){
 		links.classList.remove('active');
-		body.classList.remove('bg-oscuro');
-		setTimeout(()=>{
-			body.classList.remove('transition');
-		},500);
+		wall.classList.remove('wall-effect');
+		wall.classList.remove('wall-block');
 	}else{
 		links.classList.add('active');
-		body.classList.add('transition');
-		body.classList.add('bg-oscuro');
+		setTimeout(()=>{
+			wall.classList.add('wall-effect');
+		},1);
+		wall.classList.add('wall-block');
 	}
 });
+wall.addEventListener("click", ()=>{
+	links.classList.remove('active');
+		wall.classList.remove('wall-effect');
+		wall.classList.remove('wall-block');
+	});
 submenu[0].addEventListener("click", ()=>{
 	submenu[0].classList.toggle('active');
 	if (submenu[1].classList.contains('active')) {
